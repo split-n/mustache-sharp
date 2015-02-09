96f769c2cfa9a6c8e59061ccaca25077e3ca43b6
ri <git@tnu.jp>
2015-02-09 19:09:56 +0900

    Fix project files

--- mustache-sharp/Properties/AssemblyInfo.cs
+++ mustache-sharp/Properties/AssemblyInfo.cs
@@ -3,28 +3,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
 [assembly: AssemblyTitle("mustache-sharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A extension of the mustache text template engine for .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("truncon")]
 [assembly: AssemblyProduct("mustache-sharp")]
 [assembly: AssemblyCopyright("Copyright (C)  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("ja")]
-
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
-//
-//      メジャー バージョン
-//      マイナー バージョン 
-//      ビルド番号
-//      リビジョン
-//
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.8.1")]
+[assembly: AssemblyFileVersion("0.2.8.1")]
+[assembly: InternalsVisibleTo("mustache-sharp.test")]
